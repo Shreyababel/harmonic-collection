2a4212ac8c979a3b889d9d2732fef1c40fc0a9b6
Shreyababel <babes362@newschool.edu>
2024-10-22 00:52:49 +0000

    entry6

--- entry-5/Script.js
+++ entry-5/Script.js
@@ -1,15 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript Loaded"); // This checks if the script runs
+// for parallax effect
 
-    const car = document.createElement("div");
-    car.className = "car";
-    document.querySelector(".animation-container").appendChild(car);
+document.addEventListener('mousemove', parallax); 
+function parallax(e)
+{
+    this.querySelectorAll('.layer').forEach(layer =>
+        {
+    const speed= layer.getAttribute('data-speed') 
+    const x = (window.innerWidth - e.pageX * speed) /90
+    const y = (window.innerWidth - e.pageY * speed) /90 
+    layer.style.transform = `translateX($(x)px) translateY(${y}px)` ; 
+    layer.style.left = "-10%";
+    
+    })
+}
 
-    function moveCar() {
-        console.log("Car Animation Started"); // Check if the animation runs
-        const animationDuration = 20; // Adjust animation duration (in seconds)
-        car.style.animation = `moveCar ${animationDuration}s linear infinite`;
-    }
-
-    moveCar();
-});
+//animation fro whole page
+var t1=sgsap.timeline();
+t1.from('.center h1', {
+    x:innerWidth * -1,
+    opacity:0,
+    stagger:-0.02
+})
+t1.from('nav > a',{
+    y:-300
+})
+t1.from('nav ul li a',{
+    y:-300,
+    stagger: .2
+})
+t1.from('.line span',{
+    height: "0px",
+    stagger: .4
+})
+t1.from('.Slink a',1,{
+    opacity:0,
+    x: -300,
+    stagger:.2
+})
+t1 .from('.img img',2,{
+    opacity:0,
+    x: 10,
+    stagger:.2
+})
