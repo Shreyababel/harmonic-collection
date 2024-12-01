e47bb3129f8f098e1c44343fd438ce8bb282a13d
Shreyababel <babes362@newschool.edu>
2024-12-01 04:53:10 +0000

    final1

---
+++ entry-5/data.js
@@ -0,0 +1,85 @@
+export const mapClasses = [
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+  ];
+  export const previews = [
+    {
+      img: "image/main-1.jpg",
+      title: "Exploring Bookstores",
+      tags: "Lost in the Pages: NYC’s Best Book Nooks Await!",
+      description:
+        "New York City's bookstores are treasure troves for book lovers, offering everything from niche genres to mainstream favorites.",
+    },
+    {
+      img: "image/main-2.jpg",
+      title: "Street Art",
+      tags: "Where every wall tells a story",
+      description:
+        "Discover NYC's vibrant street art scene, where every mural, graffiti, and hidden alley tells a story of creativity, culture, and community.",
+    },
+    {
+      img: "image/main-3.jpg",
+      title: "Underrated Bakeries",
+      tags: "Sweet Secrets: NYC’s Hidden Bakery Gems",
+      description:
+        "Discover NYC's hidden culinary gems with off-the-beaten-path bakeries serving up the city's most underrated and mouthwatering baked goods.",
+    },
+    {
+      img: "image/main-4.jpg",
+      title: "Lower Manhattan Heritage",
+      tags: "Walk Through History: Discover the Heartbeat of Lower Manhattan",
+      description:
+        "Uncover the rich history, fight for freedom, and vibrant culture of Lower Manhattan through its iconic landmarks, historic sites, and diverse neighborhoods that tell the city's story.",
+    },
+    {
+      img: "image/main-5.jpg",
+      title: "Cafes",
+      tags: "Brewed in Charm: NYC’s Coziest Café Hideaways",
+      description:
+        "Uncovering underrated cafes in NYC reveals cozy, hidden gems offering unique brews and intimate atmospheres away from the bustling crowds.",
+    },
+    {
+      img: "image/main-6.jpg",
+      title: "Hidden Parks",
+      tags: "Urban Oasis: Discover NYC’s Best Kept Green Secrets",
+      description:
+        "Explore NYC's hidden parks, where serene green spaces offer a peaceful escape from the city's hustle, perfect for relaxation, nature walks, and discovering urban oases.",
+    },
+    {
+      img: "image/main-7.jpg",
+      title: "Speakeasies",
+      tags: "Prohibition Reborn: Step Into NYC’s Secret Speakeasies",
+      description:
+        "NYC speakeasies are hidden, intimate bars that transport you back to the Prohibition era, offering craft cocktails in secret, stylish settings.",
+    },
+    {
+      img: "image/main-8.jpg",
+      title: "Thrift and Vintage Stores",
+      tags: "Retro Finds, Modern Style: NYC’s Thrift & Vintage Treasures",
+      description:
+        "New York City's thrift and vintage stores are a goldmine for unique, one-of-a-kind fashion finds, from retro clothing to rare collectibles, all offering a sustainable way to shop with character.",
+    },
+    {
+      img: "image/main-9.jpg",
+      title: "Farmers Market",
+      tags: "Fresh Finds, Local Flavors: NYC’s Farmers Markets Await",
+      description:
+        "Virtual fashion collections displayed through stunning graphic design and web aesthetics.",
+    },
+    {
+      img: "image/main-10.jpg",
+      title: "All Work",
+      tags: "Minimalist Aesthetics, Fashion Forward",
+      description:
+        "Fashion forward web design encapsulating the essence of minimalist aesthetics and innovation.",
+    },
+  ];
+  
